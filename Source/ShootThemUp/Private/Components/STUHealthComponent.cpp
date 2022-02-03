817f6d7a5078a2769072e9d54f26675071fbe3ae
Vacumsio <39467630+Vacumsio@users.noreply.github.com>
2022-02-03 20:52:26 +0300

    HealthComponent, TakeDamage

---
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -0,0 +1,28 @@
+// Shoot Them Up Game. All rights reserved.
+
+#include "Components/STUHealthComponent.h"
+#include <GameFramework/Actor.h>
+
+USTUHealthComponent::USTUHealthComponent()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void USTUHealthComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Health = MaxHealth;
+
+    AActor* ComponentOwner = GetOwner();
+    if (ComponentOwner)
+    {
+        ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &USTUHealthComponent::OnTakeAnyDamage);
+    }
+}
+
+void USTUHealthComponent::OnTakeAnyDamage(
+    AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+    Health -= Damage;
+}
